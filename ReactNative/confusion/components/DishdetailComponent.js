9f4817020dbdef67169ad294c22f97f6a3f188e7
Cyberpunk <divyatejadanny@gmail.com>
2019-02-05 16:33:51 +0530

    React Navigation Part ONE
    Installed react-navigation@2.0.1;
    
    DISHES is made available in every component;
    
    MenuNavigator is created using createStackNavigator;
    
    Included navigationOptions &  { navigate }

--- ReactNative/confusion/components/DishdetailComponent.js
+++ ReactNative/confusion/components/DishdetailComponent.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Card } from 'react-native-elements';
+import { DISHES } from '../shared/dishes';
 
 function RenderDish({dish}){
-   
     return(
         <Card featuredTitle={dish.name} image={require('./images/uthappizza.png')}>
             <Text style={ {margin: 10} }>
@@ -13,17 +13,34 @@ function RenderDish({dish}){
     );
 }
 
-function Dishdetail(props){
-    if(props.dish != null){
-        return(
-            <RenderDish dish={props.dish} />
-        );
-    }
-    else {
-        return(
-            <View></View>
-        );
+class Dishdetail extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            dishes: DISHES  
+        };
+    };
+    
+    /* Including custom navOptions */
+    static navigationOptions = {
+        title: 'Dish Details'
+    };
+
+    render(){
+        
+        const dishId =  this.props.navigation.getParam('dishId', '');
+        
+        if(dishId != null){
+            return(
+                <RenderDish dish={this.state.dishes[+dishId]} />
+            );
+        }
+        else {
+            return(
+                <View>
+                </View>
+            );
+        }        
     }
 }
-
 export default Dishdetail;
