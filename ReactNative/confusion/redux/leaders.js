7ab050827b954f5ca17b182c5e46b814b3b94748
Cyberpunk <divyatejadanny@gmail.com>
2019-02-14 11:50:11 +0530

    Setting Up Redux
    baseUrl.js with Server URL;
    
    ActionTypes are defined;
    
    Action Reducers are defined in separate files with initial State and Switch Cases on Action Types;
    
    ConfigureStore is defined to return store using createStore() & to combine all reducers, combinerReducers() is used within ;
    
    ActionCreators are defined where communication with server takes place; actions are dispatched here and errors are accordingly handled;

---
+++ ReactNative/confusion/redux/leaders.js
@@ -0,0 +1,25 @@
+import * as ActionTypes from './ActionTypes';
+
+export const leaders = (state={
+        isLoading: true,
+        errMesss: null,
+        leaders: []
+    }, action) => {
+    
+    switch(action.type){
+        case ActionTypes.ADD_LEADERS:
+            return {...state, isLoading: false, errMess: null, leaders: action.payload};
+            break;
+        
+        case ActionTypes.LEADERS_LOADING:
+            return {...state, isLoading: true, errMess: null, leaders: []};            
+            break;
+            
+        case ActionTypes.LEADERS_FAILED:
+            return {...state, isLoading: false, errMess: action.payload, leaders: []]};            
+            break;
+        
+        default: 
+            return state;
+    }
+}
